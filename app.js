2a4f5417a7836cb229bf4f376a1c246e47b59851
skandunoori <98115697+skandunoori@users.noreply.github.com>
2022-11-30 17:27:11 -0500

    error message

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import mongoose from 'mongoose';
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
  || 'mongodb://127.0.0.1:27017/tuiter'
 
+console.log(CONNECTION_STRING)
 mongoose.connect(CONNECTION_STRING)
 const app = express()
 app.use(cors())
